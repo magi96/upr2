98d8ff4d29401658b146b65122f39e0b443ca035
magistela <magi_96@mail.bg>
2018-02-14 13:42:24 +0200

    **JKDFL;SJ

---
+++ magistela/src/magistela/MAGISTELA.java
@@ -0,0 +1,9 @@
+package magistela;
+
+public class MAGISTELA {
+	 public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, World");
+	        
+	 }
+}
