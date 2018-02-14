ab15b2ac63a037310a82974fd3ec610b947e58ce
magistela <magi_96@mail.bg>
2018-02-14 13:51:04 +0200

    poi

--- magistela/src/magistela/MAGISTELA.java
+++ magistela/src/magistela/MAGISTELA.java
@@ -4,7 +4,6 @@ public class MAGISTELA {
 	 public static void main(String[] args) {
 	        // Prints "Hello, World" to the terminal window.
 	        System.out.println("Hello, World");
-	        
-	             
+	              
 	 }
 }
