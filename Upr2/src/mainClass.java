622b7e1dc6141460b2327b0648e8e52b033c85e0
Dany <skate_or_die@abv.bg>
2018-02-14 14:26:54 +0200

    test

--- Upr2/src/mainClass.java
+++ Upr2/src/mainClass.java
@@ -4,7 +4,6 @@ public class mainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
 		System.out.println("Github Integration");
 
 		System.out.println("TEST Github Integration");
